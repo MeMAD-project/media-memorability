324237ddd8a692f4a2e9c424cd3a74f00738b6b3
Jorma Laaksonen <jorma.laaksonen@aalto.fi>
2021-12-03 15:56:52 +0200

    Versions for 2021.

--- PicSOM_prediction/aalto-sbatch.sh
+++ PicSOM_prediction/aalto-sbatch.sh
@@ -5,7 +5,11 @@
 
 . ./venv/bin/activate
 
-./aalto-predict.py --target $1 --hidden_size $2 --picsom_features $3
+./aalto-predict-2021.py --train $1 --test $2 --hidden_size $3 \
+			--features $4 --epochs $5
 
+#./aalto-predict-2021.py --train memento/train/short --epochs 1000
+
+# ./aalto-predict.py --target $1 --hidden_size $2 --picsom_features $3
 # ./aalto-predict-with-dev.py --epochs 100 --target $1 --hidden_size $2 --picsom_features $3
 
--- PicSOM_prediction/aalto-submit.sh
+++ PicSOM_prediction/aalto-submit.sh
@@ -6,14 +6,20 @@
 #set ff=( i3d-25-128-avg,audioset-527 )
 #set ff=( c3d-rn18-s1m-pool5-a )
 #set ff=( i3d-25-128-avg,audioset-527,c3d-rn18-s1m-pool5-a )
-set ff=( i3d-25-128-avg,audioset-527,c_in12_rn152_pool5o_d_a )
+#set ff=( i3d-25-128-avg,audioset-527,c_in12_rn152_pool5o_d_a )
+#set ff=( C3D )
+#set ff=( C3D,audioset-527 )
+#set ff=( i3d-25-128-avg )
+#set ff=( i3d-25-128-avg,audioset-527 )
+#set ff=( C3D audioset-527 C3D,audioset-527 i3d-25-128-avg i3d-25-128-avg,audioset-527 )
+#set ff=( bert3 C3D,bert3 C3D,audioset-527,bert3 i3d-25-128-avg,bert3 i3d-25-128-avg,audioset-527,bert3 )
+set ff=( i3d-25-128-pred i3d-25-128-pred,audioset-527 i3d-25-128-pred,bert3 i3d-25-128-pred,i3d-25-128-avg i3d-25-128-pred,i3d-25-128-avg,bert3 i3d-25-128-pred,i3d-25-128-avg,audioset-527,bert3 )
 
-set hh=`seq 20 20 500`
+set hh=`seq 20 20 800`
 
 foreach h ( $hh )
     foreach f ( $ff )
-	sbatch aalto-sbatch.sh short $h $f
-#	sbatch aalto-sbatch.sh long  $h $f
+	sbatch aalto-sbatch.sh trecvid/train/short // $h $f 1000
     end
 end
 
